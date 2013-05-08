c9c6fc12bcf77aaffaf3a71ee000f3629e3dd5fc
Stephen Collings <Stephen.Collings.Ext@bbc.co.uk>
2013-05-08 16:16:08 +0100

    Scrape areas

--- script/getAreas.js
+++ script/getAreas.js
@@ -1,9 +1,61 @@
 var utils = require('../lib/utils'),
     request = require('request'),
-    Country = require('ukc-models').Country;
+    Country = require('ukc-models').Country,
+    Area = require('ukc-models').Area,
+    xml2js = require('xml2js'),
+    modelsLib = require('ukc-models/lib');
 
+var addArea = function (tag, country) {
+  var data = {};
+  if (!tag.$.value) {
+    return;
+  }
+  data.id = parseInt(tag.$.value, 10);
+  data.name = tag.$.name;
+  data.country = country._id;
+  if (tag.$.lng && tag.$.lat) {
+    data.geo = [
+      parseFloat(tag.$.lng),
+      parseFloat(tag.$.lat)
+    ];
+  }
+  modelsLib.updateOrCreate(Area, data, function (err, area) {
+    country.areas.push(area._id);
+    country.save();
+    console.info('Country saved');
+  });
+};
 
-Country.findOne({id: 1}, function (err, docs) {
-  console.info(err);
-  console.info(docs);
+var fetchCountryData = function (country) {
+  utils.requestPage('/logbook/livecountry.php?id=' + country.id, function (err, response, body) {
+    body = utils.convertToUtf8(body);
+
+    var parser = new xml2js.Parser();
+    parser.parseString(body, function (err, result) {
+      result = result.country;
+      if (result.$) {
+        if (result.$.lng && result.$.lat) {
+          country.geo = [
+            parseFloat(result.$.lng),
+            parseFloat(result.$.lat)
+          ];
+        }
+
+        if (result.$.zoom) {
+          country.zoom = parseInt(result.$.zoom, 10);
+        }
+
+        country.save();
+      }
+      if (result.area) {
+        result.area.forEach(function (area) {
+          addArea(area, country);
+        });
+      }
+    });
+  });
+};
+
+Country.find({}, function (err, docs) {
+  fetchCountryData(docs[0]);
 });
