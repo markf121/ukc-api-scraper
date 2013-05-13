1410059c0a9e08632635c9910c26bffe6a7968cf
stevoland <stevoland@gmail.com>
2013-05-13 16:38:38 +0100

    Pass proxy in through config

--- app.js
+++
@@ -1,9 +0,0 @@
-var restify = require('restify'),
-    crags = require('./routes/crags');
-
-var server = restify.createServer();
-server.get('/crags/:id', crags.findById);
-
-server.listen(3000, function () {
-  console.log('%s listening at %s', server.name, server.url);
-});
