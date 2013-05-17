1ed4c6c7e0ad5f14a51053ec1e78ae8245a4c5e5
stevoland <stevoland@gmail.com>
2013-05-17 16:23:03 +0100

    Complete Guidebook scraping

--- script/getGuidebooks.js
+++ script/getGuidebooks.js
@@ -3,7 +3,8 @@ var utils = require('../lib/utils'),
     Country = models.Country,
     Guidebook = models.Guidebook,
     GuideBookScraper = require('../lib/GuideBookScraper'),
-    modelsLib = require('ukc-models/lib');
+    modelsLib = require('ukc-models/lib'),
+    async = require('async');
 
 
 var addGuidebook = function (tag, country) {
@@ -28,7 +29,31 @@ var addGuidebook = function (tag, country) {
   utils.fetchCountryData(docs, 'guidebook', addGuidebook);
 });*/
 
-var scraper = new GuideBookScraper(models);
-scraper.scrape(2, {
-  proxy: 'http://www-cache.reith.bbc.co.uk:80'
+var throttle = 5000;
+
+Guidebook.find({}, '_id', function (err, countries) {
+  console.info(countries);
+  async.eachSeries(countries, function (country, done) {
+    console.info('Scraping ' + country._id);
+    (new GuideBookScraper(models)).scrape(country._id, {
+      proxy: 'http://www-cache.reith.bbc.co.uk:80'
+    }).then(function (book) {
+      console.info('Done ' + country._id);
+      setTimeout(done, throttle);
+    }, function (err) {
+      console.info('Done ' + country._id);
+      console.error(err);
+      setTimeout(done, throttle);
+    });
+  }, function () {
+    console.info('All done');
+  });
 });
+
+/*var scraper = new GuideBookScraper(models);
+scraper.scrape(521, {
+  proxy: 'http://www-cache.reith.bbc.co.uk:80'
+}).then(function (book) {
+  console.info(book);
+  console.info('done');
+});*/
