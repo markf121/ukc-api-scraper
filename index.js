d44aa843b2ea95e2acec0b7fa81120619099eb9b
stevoland <stevoland@gmail.com>
2013-05-17 17:52:14 +0100

    Fix Guidebook scraping

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var Scraper = require('./lib/Scraper'),
     CragScraper = require('./lib/CragScraper'),
-    ClimbScraper = require('./lib/ClimbScraper');
+    ClimbScraper = require('./lib/ClimbScraper'),
+    GuidebookScraper = require('./lib/GuidebookScraper');
 
 module.exports = {
   Scraper: Scraper,
   CragScraper: CragScraper,
-  ClimbScraper: ClimbScraper
+  ClimbScraper: ClimbScraper,
+  GuidebookScraper: GuidebookScraper
 };
