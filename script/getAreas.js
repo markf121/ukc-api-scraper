5ca49e4daff12e3ff7119925d88189df27053bca
stevo <stevoland@gmail.com>
2013-06-11 22:45:26 +0100

    Fix getArea script

--- script/getAreas.js
+++ script/getAreas.js
@@ -1,34 +1,34 @@
-var models = require('ukc-models')({host: "mongodb://localhost/test"}),
-    Country = models.Country,
-    Area = models.Area,
-    modelsLib = require('ukc-models/lib'),
+var modelsLib = require('ukc-models/lib'),
     injector = require('../lib/injector');
-
-var addArea = function (tag, country) {
-  var data = {};
-  if (!tag.$.value) {
-    return;
-  }
-  data._id = parseInt(tag.$.value, 10);
-  data.name = tag.$.name;
-  data.country = country._id;
-  if (tag.$.lng && tag.$.lat) {
-    data.geo = [
-      parseFloat(tag.$.lng),
-      parseFloat(tag.$.lat)
-    ];
-  }
-  modelsLib.updateOrCreate(Area, data, function (err, area) {
-    //country.areas.push(area._id);
-    //country.save();
-    console.info('Area saved: ' + data.name);
-  });
-};
 
 
 injector.resolve(
-  function (utils) {
-    Country.find({}, function (err, docs) {
+  function (utils, config) {
+    var models = require('ukc-models')(config.get('database'));
+
+    var addArea = function (tag, country) {
+      var data = {};
+      if (!tag.$.value) {
+        return;
+      }
+      data._id = parseInt(tag.$.value, 10);
+      data.name = tag.$.name;
+      data.country = country._id;
+      if (tag.$.lng && tag.$.lat) {
+        data.geo = [
+          parseFloat(tag.$.lng),
+          parseFloat(tag.$.lat)
+        ];
+      }
+      modelsLib.updateOrCreate(models.Area, data, function (err, area) {
+        //country.areas.push(area._id);
+        //country.save();
+        console.info('Area saved: ' + data.name);
+      });
+    };
+
+
+    models.Country.find({}, function (err, docs) {
       utils.fetchCountryData(docs, 'area', addArea);
     });
   }
